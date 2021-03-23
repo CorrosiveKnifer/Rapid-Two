c827758c8c0f08dfc683234910c274a46e9a12f6
Michael Jordan <michdan25@gmail.com>
2021-03-24 12:56:38 +1300

    my scene

---
+++ Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Michael Jordan
+/// </summary>
+public class WaypointScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
