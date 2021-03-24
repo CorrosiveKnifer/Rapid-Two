84f6b7a01142269162b9185ab7ded39f2b319bae
dk23k <55791230+GrizzBe@users.noreply.github.com>
2021-03-24 13:58:15 +1300

    Started adding turret plots

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -14,6 +14,7 @@ public class CameraControl : MonoBehaviour
 
     [Header("Attachements")]
     public Camera m_Camera;
+    public GameObject m_Marker;
 
 
     // Start is called before the first frame update
@@ -28,9 +29,33 @@ public class CameraControl : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        transform.position += (transform.right * x + transform.forward * z) * fCameraMoveSpeed;
+        transform.position += (transform.right * x + transform.forward * z) * fCameraMoveSpeed * m_Camera.orthographicSize / 15;
 
         m_Camera.orthographicSize = Mathf.Clamp(m_Camera.orthographicSize - Input.mouseScrollDelta.y * fCameraZoomSpeed, 1, 15);
-        
+
+        RaycastHit[] hits;
+
+        Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 50.0f, Color.red, 0.5f);
+        hits = Physics.RaycastAll(ray.origin, ray.direction, 50.0f);
+
+        if (hits.Length != 0)
+        {
+            RaycastHit closestHit = hits[0];
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (closestHit.distance > hits[i].distance && hits[i].collider.gameObject != m_Marker)
+                    closestHit = hits[i];
+            }
+
+            m_Marker.transform.position = closestHit.point;
+
+            TurretPlot plot = closestHit.collider.gameObject.GetComponentInChildren<TurretPlot>();
+            if (plot != null)
+            {
+                plot.SpawnTurret();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/TurretPlot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretPlot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTurret(/*Can put prefab here later*/)
+    {
+        // TEMP
+
+    }
+}
