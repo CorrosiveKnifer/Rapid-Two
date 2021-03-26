8a47deb0ab31241cb60b1ca87a02206a152d2bca
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-03-26 15:28:29 +1300

    Icestuff

---
+++ Assets/Scripts/IceFlooring.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceFlooring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //checking when the bullet hits a enemy
+        if (other.tag == "Enemy")
+        {
+            //Slowing down the enemies
+            Debug.Log("Slow down");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //checking when the bullet hits a enemy
+        if (other.tag == "Enemy")
+        {
+            //putting the enemies speed back
+            Debug.Log("Speed Back");
+        }
+    }
+}
---
+++ Assets/Scripts/IcebulletScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcebulletScript : MonoBehaviour
+{
+    private float bulletdamage = 0;
+    private float BulletHealth = 10;
+    public GameObject target;
+    public GameObject IceField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+
+            GetComponent<Rigidbody>().velocity = direction.normalized * 8.0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        //checking when the bullet hits a enemy
+        if (other.tag == "Enemy")
+        {
+            BulletHealth = other.GetComponentInParent<EnemyScript>().DealDamageToEnemy(bulletdamage);
+            //if it does hit
+            if (BulletHealth == 0)
+            {
+                Effect();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void Effect()
+    {
+        GameObject IceEffect = (GameObject)Instantiate(IceField, target.transform.position, target.transform.rotation);
+    }
+
+    //setting the bullets damage
+    public void SetDamage(float damageNum)
+    {
+        bulletdamage = damageNum;
+    }
+}
