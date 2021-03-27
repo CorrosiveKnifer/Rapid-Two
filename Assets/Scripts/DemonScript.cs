5d31b55f139a9033e4aa43df6fe50b330b414682
Michael Jordan <michdan25@gmail.com>
2021-03-27 21:30:35 +1300

    my stuff

---
+++ Assets/Scripts/DemonScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonScript : MinionScript
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
