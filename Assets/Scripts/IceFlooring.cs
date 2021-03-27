15f486750ffa02dd1118fac81c87fe5d0cb06f98
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-03-27 16:44:23 +1300

    Update IceFlooring.cs

--- Assets/Scripts/IceFlooring.cs
+++ Assets/Scripts/IceFlooring.cs
@@ -7,6 +7,11 @@ using UnityEngine;
 /// </summary>
 public class IceFlooring : MonoBehaviour
 {
+    private bool isIced = false;
+    public float timer = 10.0f;
+    public List<GameObject> allCollisions = new List<GameObject>();
+    //public GameObject[] enemies;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,17 +21,30 @@ public class IceFlooring : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isIced)
+        {
+            if (timer <= 0.0f)
+            {
+                for(int i =0; i < allCollisions.Count; i++)
+                {
+                    allCollisions[i].GetComponentInParent<EnemyScript>().SetMovementMod(1.0f);
+                }
+                Destroy(gameObject);
+            }
+            timer -= 0.1f;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         //checking when the bullet hits a enemy
         if (other.tag == "Enemy")
         {
+            isIced = true;
             other.GetComponentInParent<EnemyScript>().SetMovementMod(0.5f);
             //Slowing down the enemies
             //Debug.Log(other.GetComponentInParent<EnemyScript>().MovementSpeedMod);
             Debug.Log("Slow down");
+            allCollisions.Add(other.gameObject);
         }
     }
     private void OnTriggerExit(Collider other)
@@ -37,7 +55,8 @@ public class IceFlooring : MonoBehaviour
             //putting the enemies speed back
             Debug.Log("Speed Back");
             other.GetComponentInParent<EnemyScript>().SetMovementMod(1.0f);
-           // Debug.Log(other.GetComponentInParent<EnemyScript>().MovementSpeedMod);
+            allCollisions.Remove(other.gameObject);
+            // Debug.Log(other.GetComponentInParent<EnemyScript>().MovementSpeedMod);
         }
     }
 }
