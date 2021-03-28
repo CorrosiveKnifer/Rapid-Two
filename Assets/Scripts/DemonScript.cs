3cb2e138c21a5de83a16174e706763bcb5378294
Michael Jordan <michdan25@gmail.com>
2021-03-28 16:53:22 +1300

    prefab stuff

--- Assets/Scripts/DemonScript.cs
+++ Assets/Scripts/DemonScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class DemonScript : MinionScript
 {
+    [Header("Demon Settings")]
+    public float maxHealth = 100.0f;
+    public float DetectRadius = 12.5f;
+    public float Damage;
+
+    private enum AIState { DETECT, SELECTED, HARVEST, DEPOSIT };
+    private AIState currentState;
+    private GameObject myHuntTarget;
+
     // Start is called before the first frame update
     protected override void Start()
     {
@@ -13,5 +22,28 @@ public class DemonScript : MinionScript
     protected override void Update()
     {
         base.Update();
+    }
+    private void TransitionTo(AIState newState)
+    {
+        if (currentState == newState)
+            return;
+
+        switch (newState)
+        {
+            case AIState.DETECT:
+                myHuntTarget = null;
+                break;
+            case AIState.SELECTED:
+                break;
+            case AIState.HARVEST:
+                break;
+            case AIState.DEPOSIT:
+                myHuntTarget = null;
+                break;
+            default:
+                break;
+        }
+
+        currentState = newState;
     }
 }
