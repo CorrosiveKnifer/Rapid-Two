7ffd1d574953343ef8c98eb09365540a0bf5a3c3
Michael Jordan <michdan25@gmail.com>
2021-04-01 11:28:41 +1300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -97,7 +97,8 @@ public class Player : MonoBehaviour
     public GameObject m_Demon;
     private GameObject m_selected;
 
-
+    private bool HasGainedMana = true;
+    private bool m_bhasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -182,6 +183,21 @@ public class Player : MonoBehaviour
     {
         // Mana update
         //fManaPool += Time.deltaTime * fManaRegen;
+
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+        {
+            if (!HasGainedMana)
+            {
+                fManaPool += 15.0f;
+                HasGainedMana = true;
+            }
+
+        }
+        else
+        {
+            HasGainedMana = false;
+        }
+
         if (fManaPool > fManaMaximum)
         {
             fManaPool = fManaMaximum;
@@ -326,7 +342,13 @@ public class Player : MonoBehaviour
             GameManager.instance.EnableFrame(true);
         }
 
+        if (!m_bhasStarted)
+        {
+            m_bhasStarted = true;
+            MinionSelect();
+        }
     }
+
     public Vector3 CameraPosCalculator(Vector3 _pos)
     {
         return new Vector3(_pos.x, transform.position.y, _pos.z) - transform.forward * 15.0f;
