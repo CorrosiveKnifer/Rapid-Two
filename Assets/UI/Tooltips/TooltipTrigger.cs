db88080d181022aefffcb5ba9eb42b2605943167
FetusChan <62039548+FetusChan@users.noreply.github.com>
2021-04-01 08:08:08 +1300

    Made tower tooltips
    Made tower tooltips

---
+++ Assets/UI/Tooltips/TooltipTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public string header;
+    public string cost;
+    public string content;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TooltipSystem.Show(content, cost, header);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TooltipSystem.Hide();
+    }
+}
